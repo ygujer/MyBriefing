d940c15a99f41e2432609874aa0eb5b51a7f46de
Egemen Yağcı <ygujer123@gmail.com>
2026-05-26 18:33:10 +0300

    Initial Commit

---
+++ MyBriefing/MyBriefingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBriefingApp.swift
+//  MyBriefing
+//
+//  Created by Egemen Yağcı on 26.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyBriefingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
